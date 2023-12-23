f87b83b9dcda5aa48d71ff13431908a680fdb4bd
Damian Thomas <dimitrisonic@gmail.com>
2023-12-23 13:15:10 -0500

    preserving facing direction with player movement

--- player.gd
+++ player.gd
@@ -1,7 +1,8 @@
 extends Area2D
 @export var speed = 400
 var screen_size: Vector2
-
+var upside_down = false
+var facing_back = true
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
@@ -28,5 +29,31 @@ func _process(delta):
 	position += velocity * delta
 	position = position.clamp(Vector2.ZERO, screen_size)
 
+	# controls movement animation (horizontal / vertical)
+	if abs(velocity.x) > abs(velocity.y):
+		$AnimatedSprite2D.animation = "walk"
+	else:
+		$AnimatedSprite2D.animation = "up"
+
+	# controls sprite orientation (flipped / unflipped)
+	if velocity.y > 0:
+		$AnimatedSprite2D.flip_v = true
+		upside_down = true;
+	elif velocity.y < 0:
+		$AnimatedSprite2D.flip_v = false
+		upside_down = false;
+	else:
+		$AnimatedSprite2D.flip_v = upside_down;
+		
+	if velocity.x > 0:
+		$AnimatedSprite2D.flip_h = false
+		facing_back = false;
+	elif velocity.y < 0:
+		$AnimatedSprite2D.flip_h = true
+		facing_back = true;
+	else:
+		$AnimatedSprite2D.flip_h = facing_back;			
+	
+	
 	
 
